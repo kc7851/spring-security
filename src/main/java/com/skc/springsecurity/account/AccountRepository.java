7e664466317b6fce7a754908beabf8067a801080
KiChang Shin <kc7851@gmail.com>
2020-03-06 21:46:38 +0900

    jpa 연동하여 유저 저장
    jpa, h2 의존성 추가
    
    UserService에서 UserDetailsService를 구현.

---
+++ src/main/java/com/skc/springsecurity/account/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.skc.springsecurity.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByUsername(String username);
+}
