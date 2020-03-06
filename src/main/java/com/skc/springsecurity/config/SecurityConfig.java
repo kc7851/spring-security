9863c5aafab861fadf3e40aeb167c24bbf59cc5a
KiChang Shin <kc7851@gmail.com>
2020-03-06 21:09:21 +0900

    인메모리 유저 추가
    properties에서 spring.security ~~ 로도 만들 수 있음.

--- src/main/java/com/skc/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/skc/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.skc.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http.formLogin();
         http.httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("skc").password("{noop}123").roles("USER").and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 
 }
