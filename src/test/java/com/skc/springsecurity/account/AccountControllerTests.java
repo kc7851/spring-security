0334882067131afa31bf1aa3b13f80ced12eb125
KiChang Shin <kc7851@gmail.com>
2020-03-08 01:17:14 +0900

    스프링 시큐리티 테스트 2

--- src/test/java/com/skc/springsecurity/account/AccountControllerTests.java
+++ src/test/java/com/skc/springsecurity/account/AccountControllerTests.java
@@ -7,9 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,9 +24,12 @@ class AccountControllerTests {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private AccountService accountService;
+
     @Test
     @WithAnonymousUser
-    public void index_anonymous() throws Exception{
+    public void index_anonymous() throws Exception {
         mvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk());
@@ -31,7 +37,7 @@ class AccountControllerTests {
 
     @Test
     @WithUser
-    public void index_user() throws Exception{
+    public void index_user() throws Exception {
         mvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk());
@@ -39,18 +45,66 @@ class AccountControllerTests {
 
     @Test
     @WithMockUser(username = "skc", roles = "USER")
-    public void admin_user() throws Exception{
+    public void admin_user() throws Exception {
         mvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isForbidden());
     }
 
     @Test
-    public void admin_admin() throws Exception{
+    public void admin_admin() throws Exception {
         mvc.perform(get("/admin")
-        .with(user("skc").roles("ADMIN")))
+                .with(user("skc").roles("ADMIN")))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional
+    public void login() throws Exception {
+        String username = "skc";
+        String password = "123";
+        Account user = createUser(username, password);
+
+        mvc.perform(formLogin()
+                .user(user.getUsername()).password(password)
+        )
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @Transactional
+    public void login2() throws Exception {
+        String username = "skc";
+        String password = "123";
+        Account user = createUser(username, password);
+
+        mvc.perform(formLogin()
+                .user(user.getUsername()).password(password)
+        )
+                .andExpect(authenticated());
+    }
+
+    private Account createUser(String username, String password) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole("USER");
+
+        return accountService.createNew(account);
+    }
+
+    @Test
+    @Transactional
+    public void loginFail() throws Exception {
+        String username = "skc";
+        String password = "123";
+        Account user = createUser(username, password);
+
+        mvc.perform(formLogin()
+                .user(user.getUsername()).password("12345")
+        )
+                .andExpect(unauthenticated());
     }
 
 }
