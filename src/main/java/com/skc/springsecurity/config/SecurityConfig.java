eeceb63c5c87d2e4a455d0ab0f01fb58f53c12e9
KiChang Shin <kc7851@gmail.com>
2020-03-11 22:13:13 +0900

    logout config
    LogoutFilter.java 에서 처리

--- src/main/java/com/skc/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/skc/springsecurity/config/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
 
+        http.logout().logoutSuccessUrl("/");
+
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
