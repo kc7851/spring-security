0c1d4f5a76001f1cf2c4f82300e3889ace236063
KiChang Shin <kc7851@gmail.com>
2020-03-14 01:26:22 +0900

    SessionManagerFilter

--- src/main/java/com/skc/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/skc/springsecurity/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -32,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/", "/info", "/account/**", "/signup").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
+
         http.formLogin()
                 .loginPage("/custom/login").permitAll()
         ;
@@ -42,6 +44,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/")
         ;
 
+        http.sessionManagement()
+                .sessionFixation()
+                    .changeSessionId();
+//                .maximumSessions(1)
+//                    .expiredUrl("/") // 세션이 만료되었을 때 리다이렉트 시킬 url
+//                    .maxSessionsPreventsLogin(true); //false로 되어있으면 기존 세션이 있을 때 로그인 허용 안함.
+
+        // REST api를 다룰 때 사용. formLogin방식에는 부적
+//        http.sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
